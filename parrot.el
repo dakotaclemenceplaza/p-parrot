5012f0f88c20bbc000415c6c51d6d708fb04bbdd
A D <asdkffdsfj@gmail.com>
2017-07-15 11:46:22 +0400

    Add colors

--- parrot.el
+++ parrot.el
@@ -29,6 +29,9 @@
     (forward-line (- winh 5))
     (insert (make-string (- (/ winw 2) 5) ? ))
     (insert "C-g to stop")
+
+    (colorize-appropriate start)
+
     ;show everything
     (redisplay)
     ;keeping track of frames
@@ -39,9 +42,19 @@
       (setq start (+ start length) end (+ end length)))
     (sleep-for speed)))
 
+(defun colorize-appropriate (start)
+  (let ((choose-color
+	 (lambda (color)
+	   (add-face-text-property (point-min) (point-max) `(:foreground ,color)))))
+    (cond ((or (= start 0) (= start 9162)) (funcall choose-color "#FDFA00"))
+	  ((= start 1018) (funcall choose-color "#00F800"))
+	  ((= start 2036) (funcall choose-color "#00A4A2"))
+	  ((= start 3054) (funcall choose-color "#4179B2"))
+          ((= start 4072) (funcall choose-color "#555AB4"))
+	  ((= start 5090) (funcall choose-color "#C22E9C"))
+	  ((= start 6108) (funcall choose-color "#E02C98"))
+	  ((= start 7126) (funcall choose-color "#FE40FD"))
+	  ((= start 8144) (funcall choose-color "#E07898")))))
+
 ;for test
 (start-parrot)
-
-;1018
-
-(* 1018 4)
