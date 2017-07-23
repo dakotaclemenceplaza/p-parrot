db55d75b4de11a030492aa7ae174f7aee1c079d0
A D <asdkffdsfj@gmail.com>
2017-07-23 19:08:56 +0400

    Start bringing back single parrots. Change main name to p-parrots

--- parrot.el
+++ parrot.el
@@ -1,40 +1,22 @@
-(defun start-parrot (&optional arg)
+(defun p-parrot (&optional arg)
   "Party Parrot"
   (interactive "sf for fast, s for slow, g for goth, w for wave: ")
-  (setq goth nil slow nil wave nil speed 0.07 frame 0)
+  (setq goth nil slow nil wave nil speed 0.07 frames (list (list 0 10)))
   (cond ((string= arg "f") (setq speed 0.03))
 	((string= arg "s") (setq slow 1))
 	((string= arg "g") (setq goth 1))
 	((string= arg "w") (setq wave 1)))
-  (setq frames (list (list 0 10) (list 2 10) (list 4 10) (list 6 10) (list 8 10) (list 0 10)))
+  (if wave (setq slow 1 frames (list (list 0 10) (list 2 10) (list 4 10) (list 6 10) (list 8 10) (list 0 10))))
   (switch-to-buffer "Parrot")
   ;window sizes
   (setq winh (window-height) winw (window-width))
-  (setq slow-count 10)
   (while 1
     (erase-buffer)
     (draw-parrot)
-    ;make more newlines for text
-;    (newline (- (/ winh 2) 5))
-    ;put text before the bottom end 
-;    (goto-char (point-min))
-;    (forward-line (- winh 5))
-;    (insert (make-string (- (/ winw 2) 5) ? ))
-;    (insert "C-g to stop")
-    ;colorize
+;   colorize
 ;    (colorize-appropriate)
     ;show everything
     (redisplay) 
-    ;keeping track of frames
-    ;if slow, then skip changing frame by setting it to previous frame before final increment
-    ;keep skipping on fourth frame for slow-count times
-;    (if (and slow (= frame 4) (> slow-count 0)) (setq frame (1- frame)
-;						      slow-count (1- slow-count)))
-    ;after skipping change need to skip the fourth frame to let frames increment further
-;    (if (and (= slow-count 0) (= frame 3)) (setq frame 5 slow-count 10))
-;    (if (= frame 9)
-;	(setq frame 0)
-;      (setq frame (1+ frame)))
     (sleep-for speed)))
 
 (defun draw-parrot ()
@@ -51,25 +33,34 @@
 			   ((= (car parrotframe) 9) frame-nine))))
 	(draw-line
 	 (lambda (line-from-frame)
-;	   (insert (make-string (- (/ winw 2) 25) ? ))
-	   (end-of-line)
+	   (and (not wave) (insert (make-string (- (/ winw 2) 25) ? )))
+	   (and wave (end-of-line))
 	   (insert line-from-frame)
-	   (or (= (forward-line) 0) (newline))))
+	   (and wave (or (= (forward-line) 0) (newline)))
+	   (and (not wave) (newline))))
 	(parlist (mapcar return-frame frames))
+	;keeping track of frames
 	(change-frame
 	 (lambda (frame)
 	   (cond ((and slow (= (car frame) 4) (> (car (cdr frame)) 0)) (list (car frame) (1- (car (cdr frame)))))
 		 ((and (= (car (cdr frame)) 0) (= (car frame) 3)) (list 4 10))
 		 ((= (car frame) 9) (list 0 (car (cdr frame))))
 		 (t (list (1+ (car frame)) (car (cdr frame))))))))
-    (mapc (lambda (pf) (mapc draw-line pf) (cond ((< (point) 2500) (goto-char (point-min)))
+    (and wave (mapc (lambda (pf) (mapc draw-line pf) (cond ((< (point) 2500) (goto-char (point-min)))
 						 ((< (point) 3500) (goto-char (point-max)))
 						 (t (forward-line -19))))
-	  parlist)
+		   parlist))
+    (and (not wave)
+	 (progn (newline (- (/ winh 2) 10))
+		(mapc draw-line (car parlist))
+		;make more newlines for text
+		(newline (- (/ winh 2) 5))
+		;put text before the bottom end 
+		(goto-char (point-min))
+		(forward-line (- winh 5))
+		(insert (make-string (- (/ winw 2) 5) ? ))
+		(insert "C-g to stop")))
     (setq frames (mapcar change-frame frames))))
-
-					;    (newline (- (/ winh 2) 10))
-
 
 ;function for color
 ;choose-color holds a lambda which takes a color argument depending on frame
