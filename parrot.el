65ab6168e262797ec3df462078920722fe5b1011
A D <asdkffdsfj@gmail.com>
2017-07-30 05:50:22 +0400

    Colorize wave parrots; put colors together with parrotframes

--- parrot.el
+++ parrot.el
@@ -13,8 +13,6 @@
   (while 1
     (erase-buffer)
     (draw-parrot)
-;   colorize
-;    (colorize-appropriate)
     ;show everything
     (redisplay) 
     (sleep-for speed)))
@@ -33,11 +31,16 @@
 			   ((= (car parrotframe) 9) frame-nine))))
 	(draw-line
 	 (lambda (line-from-frame)
-	   (and (not wave) (insert (make-string (- (/ winw 2) 25) ? )))
-	   (and wave (end-of-line))
-	   (insert line-from-frame)
-	   (and wave (or (= (forward-line) 0) (newline)))
-	   (and (not wave) (newline))))
+	   (if wave (progn (end-of-line)
+			   (setq p-min (point))
+			   (insert line-from-frame)
+			   (setq p-max (point))
+			   (or (= (forward-line) 0)
+			       (newline))
+			   (colorize-appropriate))
+	     (progn (insert (make-string (- (/ winw 2) 25) ? ))
+		    (insert line-from-frame)
+		    (newline)))))
 	(parlist (mapcar return-frame frames))
 	;keeping track of frames
 	(change-frame
@@ -46,36 +49,27 @@
 		 ((and (= (car (cdr frame)) 0) (= (car frame) 3)) (list 4 10))
 		 ((= (car frame) 9) (list 0 (car (cdr frame))))
 		 (t (list (1+ (car frame)) (car (cdr frame))))))))
-    (and wave (mapc (lambda (pf) (mapc draw-line pf) (cond ((< (point) 2500) (goto-char (point-min)))
-						 ((< (point) 3500) (goto-char (point-max)))
-						 (t (forward-line -19))))
-		   parlist))
-    (and (not wave)
-	 (progn (newline (- (/ winh 2) 10))
-		(mapc draw-line (car parlist))
-		;make more newlines for text
-		(newline (- (/ winh 2) 5))
-		;put text before the bottom end 
-		(goto-char (point-min))
-		(forward-line (- winh 5))
-		(insert (make-string (- (/ winw 2) 5) ? ))
-		(insert "C-g to stop")))
+    ;end of let*
+    (if wave (mapc (lambda (pf) (setq current-color (car pf)) (mapc draw-line (cdr pf)) (cond ((< (point) 2500) (goto-char (point-min)))
+											      ((< (point) 3500) (goto-char (point-max)))
+											      (t (forward-line -19))))
+		   parlist)
+      ;else
+      (progn (newline (- (/ winh 2) 10))
+	     (mapc draw-line (cdr (car parlist)))
+	     ;make more newlines for text
+	     (newline (- (/ winh 2) 5))
+	     ;put text before the bottom end 
+	     (goto-char (point-min))
+	     (forward-line (- winh 5))
+	     (insert (make-string (- (/ winw 2) 5) ? ))
+	     (insert "C-g to stop")
+	     (setq current-color (car (car parlist)) p-min (point-min) p-max (point-max))
+	     (colorize-appropriate)))
+    ;end of if-else
     (setq frames (mapcar change-frame frames))))
 
-;function for color
-;choose-color holds a lambda which takes a color argument depending on frame
 (defun colorize-appropriate ()
   (if goth
       (add-face-text-property (point-min) (point-max) '(:foreground "#787B80" :weight bold))
-    (let ((choose-color
-	   (lambda (color)
-	     (add-face-text-property (point-min) (point-max) `(:foreground ,color)))))
-      (cond ((or (= frame 0) (= frame 9)) (funcall choose-color "#FDFA00"))
-	    ((= frame 1) (funcall choose-color "#00F800"))
-	    ((= frame 2) (funcall choose-color "#00A4A2"))
-	    ((= frame 3) (funcall choose-color "#4179B2"))
-	    ((= frame 4) (funcall choose-color "#555AB4"))
-	    ((= frame 5) (funcall choose-color "#C22E9C"))
-	    ((= frame 6) (funcall choose-color "#E02C98"))
-	    ((= frame 7) (funcall choose-color "#FE40FD"))
-	    ((= frame 8) (funcall choose-color "#E07898"))))))
+    (add-face-text-property p-min p-max `(:foreground ,current-color))))
